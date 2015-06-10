c349f5f00aa033820d5e7027be83059ffe01d397
mannion9 <galway1212@hotmail.com>
2015-06-10 16:21:41 -0700

    Completed
    Generalized completely and finished including a test of the PDF = x ^2.

--- Normalizing-a-PDF.py
+++ Normalizing-a-PDF.py
@@ -7,45 +7,79 @@ Created on Wed Jun 10 12:55:05 2015
 
 import scipy as sp
 import numpy as np
+import random as r
 import matplotlib.pyplot as plt 
+from bisect import bisect_left 
+
+check = 1
 
 a = 0
 b = 1
-N = 100
-x = np.linspace(a,b,N)
+N = 1000
 
 ''' Generalized PDF normalization '''
 
-def PDF(x):
-    return x**2
+def CreateCDF(pdf,a,b,N):
+    ''' Generalized CDF Creation from Normalized PDF'''
+    x = np.linspace(a,b,N)
+    def Normalize(pdf):
+        y = pdf(x)
+        I = sp.integrate.simps(y,x)
+        return 1/I
     
-def Normalize(PDF):
-    y = PDF(x)
-    I = sp.integrate.simps(y,x)
-    return 1/I
-
-cont = Normalize(PDF)
-
-def NormPDF(x):
-    return cont * PDF(x)
-
-''' Generalized CDF Creation from Normalized PDF'''
-
-def CDF(x):
-    return np.cumsum(NormPDF(x))/N
+    cont = Normalize(pdf)
     
-CDF = CDF(x)
+    def NormPDF(x):
+        return cont * pdf(x)
+        
+    def CDF(x):
+        return np.cumsum(NormPDF(x))/N
+        
+    return CDF(x) , x
 
-
+def Choice(cdf,x):
+    ''' Returns the the chosen value (corresponding to the CDF) that are closest to the choice random value (ranging from 0 to 1)'''
+    choice = r.random()
+    pos = bisect_left(cdf,choice)
+    if pos == 0:
+        pos_next = 1
+    if pos == len(cdf)-1:
+        pos_next = len(cdf)-2
+    else:
+        if abs(choice-cdf[pos-1]) < abs(choice-cdf[pos+1]):
+            pos_next = pos-1
+        else:
+            pos_next = pos+1     
+    ''' linear interpolation between these points to esitmate the result '''
+    m = abs(cdf[pos]-cdf[pos_next])/abs(x[pos]-x[pos_next])
+    value = ((choice - cdf[pos])/m) + x[pos]
+    return value
 
 
 ''' Check '''
-plt.scatter(x,CDF,c='r')
-def check(x):
-    return x**3
-plt.plot(x,check(x),c='b')
-plt.xlabel('Variable x ')
-plt.ylabel('Cumulative density')
-plt.title('Cumulative densty function')
-plt.legend()
-plt.axis([0,1,0,1])
+if check == 1:
+    def Test_1(x):
+        return x**2
+    CDF_test_1, x_1 = CreateCDF(Test_1,a,b,N)
+    
+    plt.figure(1)
+    plt.scatter(x_1,CDF_test_1,c='r')
+    def check(x):
+        return x**3
+    plt.plot(x_1,check(x_1),c='b')
+    plt.xlabel('Variable x ')
+    plt.ylabel('Cumulative density')
+    plt.title('Cumulative densty function')
+    plt.axis([0,1,0,1])
+    
+    choices = []    
+    for i in range(10000):
+        choices.append(Choice(CDF_test_1,x_1))
+    plt.figure(2)
+    plt.hist(choices)
+    plt.xlabel('Chosen random variable')
+    plt.ylabel('Counts')
+    plt.title('Histogram of chosen variable with a given PDF 10,000 runs')
+    
+    
+    
