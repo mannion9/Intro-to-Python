4ff20d14abe099f11360e3a3189e2f3da90cf371
mannion9 <galway1212@hotmail.com>
2015-07-28 16:33:42 -0700

    Numerical integration in many dimensions with trapezoid rule
    I wrote this program to integrate in multi dimensions using the
    trapezoid rule.

---
+++ Integration/MultipleVariableLoopMethod.py
@@ -0,0 +1,40 @@
+import numpy as np
+from scipy.integrate import dblquad 
+from time import time as tic
+
+def func(x,t):
+    return x**2 + t**2
+
+N = 10000
+start = tic()
+for i in range(N):
+    I = dblquad(func,0,1,lambda t: 0,lambda t: 1)
+print('Numpy function:',tic()-start)
+
+
+
+def trapazoid(function,x):
+    y = function(x)
+    del_x = (x[-1]-x[0])/(2*(len(x)-1))
+    A = 2*np.identity(len(x))
+    A[0][0] , A[-1][-1] = 1 , 1
+    I = sum(A.dot(y))*del_x
+    return (I)
+    
+
+def trapazoid_2_d(function,x_1,x_2):
+    I = 0
+    del_x = (x_1[-1]-x_2[0])/(2*(len(x_1)-1))
+    A = 2*np.identity(len(x_1))
+    A[0][0] , A[-1][-1] = 1 , 1    
+    for t in x_2:
+        y = function(x_1,t)
+        I += sum(A.dot(y))*del_x
+    return I*((x_2[-1]-x_2[0])/len(x_2))
+
+x_1 = np.linspace(0,1,100)
+x_2 = np.linspace(0,1,100)
+start = tic()
+for i in range(N):
+    I = trapazoid_2_d(func,x_1,x_2)
+print("Me:",tic()-start)
