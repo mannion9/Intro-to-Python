c5e7a5e40fb68c92c4c17681b050e5cd8beb680a
galway1212@hotmail.com <owenmannion@owens-mbp.attlocal.net>
2015-08-09 18:09:17 -0700

    GaussianElmination

--- GuassianElimination.py
+++ GuassianElimination.py
@@ -1,17 +1,35 @@
 ''' Guassian elimination for the system Ax=b where
     A is a NxN matrix, x is a length N vector to be
     solved for and b is the lenght N vector'''
-import PypyNumpy as np
 
-A = [ [1,1,1],[0,1,0],[0,0,1]]
+A = [[1,0,0],[0,1,0],[0,0,1]]
 b = [4,5,6]
 
-n = len(A[0])
-for k in range(1,n-1):
-    for i in range(k+1,n):
-        l = A[i][k]/A[k][k]
-        for j in range(k+1,n):
-            A[i][j] -= l*A[k][j]
-        b[i] -= l*b[k]
-for k in range(
-        
+
+def Solve(A,b):
+    for row in range(len(A)):
+        A[row].append(b[row])
+    #eliminate columns
+    for col in range(len(A[0])):
+        for row in range(col+1, len(A)):
+            r = [(rowValue * (-(A[row][col] / A[col][col]))) for rowValue in A[col]]
+            A[row] = [sum(pair) for pair in zip(A[row], r)]
+    #now backsolve by substitution
+    x = []
+    A.reverse() #makes it easier to backsolve
+    for sol in range(len(A)):
+        if sol == 0:
+            x.append(A[sol][-1] / A[sol][-2])
+        else:
+            inner = 0
+            #substitute in all known coefficients
+            for i in range(sol):
+                inner += (x[i]*A[sol][-2-i])
+            #the equation is now reduced to ax + b = c form
+            #solve with (c - b) / a
+            x.append((A[sol][-1]-inner)/A[sol][-sol-2])
+    x.reverse()
+    return x
+
+sol = Solve(A,b)
+print(sol)
